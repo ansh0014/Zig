100e97dece44ba03d03f49af2b07ee7e8b322532
ansh0014 <anshul.jagota@yahoo.com>
2026-02-22 18:41:57 +0530

    add some file

---
+++ integer_literals.zig
@@ -0,0 +1,12 @@
+const decimal_int=9822;
+const hex_int=0xff;
+const another_hex_int=0xFF;
+const octal_int=0o755;
+const binary_int=0b11111000;
+// underscores may be place between tow digits as visual separator
+const one_billion=1_00_000_000;
+
+const binary_mask=0b1_111_1111;
+const permissions=0o7_5_5;
+const big_address=0xFF80_0000_0000_0000;
+
